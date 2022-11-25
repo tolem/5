cb49f25b1525eb020cd8d005b128ea76c65a60da
Lami <olamide2olowoniyi@gmail.com>
2022-11-25 18:44:47 -0500

    added api framework

--- project5/project5/settings.py
+++ project5/project5/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
