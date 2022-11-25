35a0027ee1b177be3571690eb32eaf1415ef0adc
Lami <olamide2olowoniyi@gmail.com>
2022-11-25 01:24:54 -0500

    added env files

--- project5/project5/settings.py
+++ project5/project5/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+env = environ.Env()
+environ.Env.read_env()
